a6c8912eaca9a6d5bf41cbd0a22c0344fe775d59
Arif <mohd.arif@wavenetcorp.com>
2021-08-06 01:17:31 +0530

    function and method intro

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    // template:'<h2>i am template</h2>'
+    data() {
+        return {
+            name:'arif',
+            email:"we mail"
+        }
+    }
+})
+
+app.mount('#app');
